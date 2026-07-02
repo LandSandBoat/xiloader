5e660533598f8cd1cde8db00f3362128ba7fa8d7
WinterSolstice8 <60417494+wintersolstice8@users.noreply.github.com>
2026-07-02 06:35:26 -0600

    Bump mbedtls version

--- cmake/mbedtls.cmake
+++ cmake/mbedtls.cmake
@@ -1,7 +1,7 @@
 CPMAddPackage(
   NAME mbedtls
   GITHUB_REPOSITORY Mbed-TLS/mbedtls
-  GIT_TAG mbedtls-3.6.5
+  GIT_TAG mbedtls-3.6.6
   OPTIONS
     "ENABLE_PROGRAMS OFF"
     "ENABLE_TESTING OFF"
